97e20f23c7bfb427c3a901cb0d815660b6d2e69e
Kamelz <kamel1145@gmail.com>
2018-09-23 16:31:59 +0300

    Styled attribute form

--- templates/view.php
+++ templates/view.php
@@ -1,48 +1,152 @@
 <?php
- include plugin_dir_path( __FILE__ ) . '../includes/http.php';
+include plugin_dir_path( __FILE__ ) . '../includes/http.php';
+$categories = get_categories(['exclude'=>1 ,'hide_empty' => FALSE]);
 ?>
+<style type="text/css">
+.delete-btn{
+	background-color: #ad1515;
+    color: white;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+}
 
+.update-btn{
+	background-color: #2ead15;
+    color: white;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+}
 
+.submit-btn{
+	background-color: #1593ad;
+    color: white;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+}
 
+.container {
+width: 80%;
+max-width: 1200px;
+margin: 0 auto;
+}
+.container * {
+box-sizing: border-box;
+}
+.flex-outer,
+.flex-inner {
+list-style-type: none;
+padding: 0;
+}
+.flex-outer {
+max-width: 800px;
+margin: 0 auto;
+}
+.flex-outer li,
+.flex-inner {
+display: flex;
+flex-wrap: wrap;
+align-items: center;
+}
+.flex-inner {
+padding: 0 8px;
+justify-content: space-between;
+}
+.flex-outer > li:not(:last-child) {
+margin-bottom: 20px;
+}
+.flex-outer li label,
+.flex-outer li p {
+padding: 8px;
+letter-spacing: .09em;
+text-transform: uppercase;
+}
+.flex-outer > li > label,
+.flex-outer li p {
+flex: 1 0 120px;
+max-width: 220px;
+}
+.flex-outer > li > label + *,
+.flex-inner {
+flex: 1 0 220px;
+}
+.flex-outer li p {
+margin: 0;
+}
+.flex-outer li input:not([type='checkbox']),
+.flex-outer li textarea {
+padding: 15px;
+}
+.flex-outer li button {
+margin-left: auto;
+padding: 8px 16px;
+border: none;
+background: #333;
+color: #f2f2f2;
+text-transform: uppercase;
+letter-spacing: .09em;
+border-radius: 2px;
+}
+.flex-inner li {
+width: 100px;
+}
+</style>
 <table class="wp-list-table widefat fixed ">
 	<thead>
 		<tr>
 			<th class="manage-column column-author">#</th>
 			<th class="manage-column column-author">Attribute</th>
+			<th class="manage-column column-author">URL</th>
+			<th class="manage-column column-author">Overview</th>
+			<th class="manage-column column-author">Categories</th>
+			<th class="manage-column column-author">Action</th>
 		</tr>
 	</thead>
 	<tbody>
-
+		<?php
+				(new Attribute)->render();
+		?>
 	</tbody>
 </table>
 <br>
-<?php  
-	foreach (get_categories(['exclude'=>1 ,'hide_empty' => FALSE]) as $category) {
-	}
-?>
-<form method="post" action="" enctype="multipart/form-data">
-
-
-    <label for="attribute">Attribute:</label>
-    <input type="text" name='name' id="attribute" placeholder="Name">
-	
-	<label for="url">URL or Website:</label>
-    <input type="text" name='url' id="url" placeholder="Name">
-	
-	<label for="logo">logo:</label>
-    <input type="file" name='logo' id="logo" placeholder="Name">
-
-	<label for="overview">Overview:</label>
-    <textarea id="overview" name="overview"></textarea>
-
-    <select multiple="true" name="categories[]">
-    	<option value="">Select on or more categories</option>
-    	<?php foreach(get_categories(['exclude'=>1 ,'hide_empty' => FALSE]) as $category){?>
-    		<option value="<?php echo $category->term_id?>"> <?php echo $category->name; ?></option>
-    	<?php }?>	
-    </select>
-
-	<input type="submit" name='create_attribute' value='Submit'/>
-
-</form>
-
+<div id="poststuff">
+	<div class="postbox">
+		<h2 style='cursor:auto'class="hndle ui-sortable-handle"><span>Add Attributes</span></h2>
+		<div class="container">
+			<form method="post" class='flex' action="" enctype="multipart/form-data">
+				<ul class="flex-outer">
+					<li>
+						<label for="attribute">Attribute:</label>
+						<input type="text" name='name' id="attribute" placeholder="Attribute">
+					</li>
+					<li>
+						<label for="url">URL or Website:</label>
+						<input type="text" name='url' id="url" placeholder="URL">
+					</li>
+					
+					<li>
+						<label for="overview">Overview:</label>
+						<textarea id="overview" name="overview"></textarea>
+					</li>
+					<li>
+						<label >Select on or more categories</label>
+						<select multiple="true" id="categories" name="categories[]">
+							<?php foreach($categories as $category){?>
+							<option value="<?php echo $category->term_id?>"> <?php echo $category->name; ?></option>
+							<?php }?>
+						</select>
+					</li>
+					<li>
+						<label for="logo">logo:</label>
+						<input type="file" name='logo' id="logo" placeholder="Logo">
+					</li>
+					<li>
+						<input type="submit" name='create_attribute' class='submit-btn' value='Submit'/>
+					</li>
+				</ul>
+			</form>
+		</div>
+	</div>
+</div>
