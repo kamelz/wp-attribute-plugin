80aeff63187df130402c064a659d8c5a95f19cca
Kamelz <kamel1145@gmail.com>
2018-09-23 16:31:03 +0300

    file loader

--- attribute-grouper.php
+++ attribute-grouper.php
@@ -33,13 +33,7 @@ if(! function_exists('add_action')){
  */
 define( 'ATTRIBUTE_TAGGER_VERSION', '1.0.0' );
 
-require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/Model/BaseModel.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/Model/Attribute.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/Model/AttributeCategory.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-moderator.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-attribute-grouper-activator.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-attribute-grouper-deactivator.php';
+require_once plugin_dir_path( __FILE__ ) . 'loader.php';
 
 
 if(class_exists('AttributeGrouperActivator')){
---
+++ loader.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/Model/BaseModel.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/Model/Attribute.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/Model/AttributeCategory.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-moderator.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-attribute-grouper-activator.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-attribute-grouper-deactivator.php';
